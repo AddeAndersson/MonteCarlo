e622b55900d603015f55512206a7799ca7c45c30
Adrian <adde.a@hotmail.com>
2019-09-17 10:23:23 +0200

    Test 2

--- MonteCarlo/Scene.cpp
+++ MonteCarlo/Scene.cpp
@@ -1,5 +1,6 @@
 #include "Scene.h"
-
+#include <iomanip>
+#include <iostream>
 
 
 Scene::Scene()
@@ -9,4 +10,12 @@ Scene::Scene()
 
 Scene::~Scene()
 {
+}
+
+
+using namespace std;
+int main()
+{
+	cout << "Hej" << endl;
+	return 0;
 }
